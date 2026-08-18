5a8d30cb66fd6a090874c9cb93edc64252fefe49
Pavel <77823243+HydepX@users.noreply.github.com>
2026-08-18 12:44:40 +0700

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,7 @@
 from .common import router as common_router
 from .shop import router as shop_router
 from .support import router as support_router
+from .nlu_handler import router as nlu_router
 from .fallbacks import router as fallbacks_router
 
 
@@ -8,5 +9,6 @@ all_routers = [
     common_router,
     shop_router,
     support_router,
+    nlu_router,
     fallbacks_router,
 ]
